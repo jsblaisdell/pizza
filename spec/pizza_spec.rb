51c752cc5aa2011cda1a50f7915a7ead7627edc8
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-11 10:57:16 -0500

    add failing test for no toppings

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,7 +4,20 @@ require 'rspec'
 require_relative '../pizza'
 
 describe Pizza::Pie do
-
+  describe '.initialize' do
+    it 'records the toppings' do
+      toppings = [
+        Pizza::Topping.new('mushroom', vegetarian: true),
+        Pizza::Topping.new('pepperoni')
+      ]
+      pizza = Pizza::Pie.new(toppings)
+    end
+    it 'if the pizza has no toppings, defaults toppings to cheese only' do
+      pizza = Pizza::Pie.new
+      expect(pizza.toppings.size).to eq(1)
+      expect(pizza.toppings.first.name).to eq('cheese')
+    end
+  end
 end
 
 describe Pizza::Topping do
